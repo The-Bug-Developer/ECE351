0980a8ec172c098075dbd6a1361de3f7a5446e07
The-Bug-Developer <delu8015@vandals.uidaho.edu>
2023-04-26 21:14:37 -0700

    lab10 Finish

--- ECE351/Lab10/Lab10.py
+++ ECE351/Lab10/Lab10.py
@@ -10,7 +10,7 @@ import numpy as np                                             #
 import matplotlib . pyplot as plt                              #
 import scipy as sp                                             #
 import scipy . signal as sig                                   #
-import pandas as pd                                            #                                               #
+import pandas as pd                                            #                                               
 import time                                                    #
 import math                                                    #
 import control as con                                          #
@@ -30,36 +30,31 @@ C=100e-9
 magma = 10*np.log((w/(R*C)) / np.sqrt( (w/(R*C))**2 + (1/(L*C)-w**2)**2))
 angel = ((np.pi/2) - np.arctan( (w/(R*C)) / (-1*(w**2) + (1/(L*C))) )) * (180/np.pi)
 
-plt.figure(figsize=(10,7))
+plt.figure(figsize=(20,10))
 plt.subplot(2,1,1) #Top Figure
 plt.grid()
 plt.semilogx(w, magma)
-plt.title("Pre caclulated")
+plt.title("Hand caclulated")
 plt.subplot(2,1,2)
 plt.grid()
 plt.semilogx(w, angel)
 plt.show()
 
-#----------------------------TASK 1, PART 2------------#
 num = [1/(R*C),0]
 den = [1,1/(R*C),1/(L*C)]
 sys = sig.TransferFunction(num,den)
 ang,mag,phase = sig.bode(sys,w)
 
-plt.figure(figsize=(10,7))
+plt.figure(figsize=(20,10))
 plt.subplot(2,1,1) 
 plt.grid()
 plt.semilogx(ang, mag)
-plt.title("Python Calculated")
 plt.subplot(2,1,2)
 plt.grid()
 plt.semilogx(ang, phase)
-plt.show()
-
 
 sys = con.TransferFunction(num,den)
 _ = con.bode(sys, w, dB=True, Hz=True, deg=True, Plot=True)
-
 
 tea = 1e-7
 t = np.arange(0, 0.01+tea, tea)
@@ -68,13 +63,12 @@ x = (np.cos(2*np.pi*100*t)+ np.cos(2*np.pi*3024*t) + np.sin(2*np.pi* 50000*t))
 numZ, denZ = sig.bilinear(num, den, 1/tea)
 xFiltered = sig.lfilter(numZ, denZ, x)
 
-plt.figure(figsize=(10,7))
+plt.figure(figsize=(20,7))
 plt.subplot(2,1,1)
 plt.grid()
-plt.plot(t,x)
-plt.title("Unfiltered Signal")
+plt.plot(t,x, color = 'purple')
+plt.title("Sample Signal")
 plt.subplot(2,1,2)
-plt.plot(t,xFiltered)
-plt.title("Filtered Signal")
+plt.plot(t,xFiltered, color = 'red')
+plt.title("Post Filtered Signal")
 plt.grid()
-plt.show()
